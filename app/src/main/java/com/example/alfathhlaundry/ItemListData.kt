e0b038023e45f9309e983f85255bdf3b4fa81db2
jihanmajida <jeehan.majeeda@gmail.com>
2026-02-20 13:47:49 +0700

    add layout showdata, itemshowdata, item add data cust

--- app/src/main/java/com/example/alfathhlaundry/ItemListData.kt
+++ app/src/main/java/com/example/alfathhlaundry/ItemListData.kt
@@ -1,8 +1,8 @@
 package com.example.alfathhlaundry
 
-data class ItemListData {
+data class ItemListData (
     val berat: String,
     val judul: String,
     val nama: String,
     var status: Boolean = false
-}
+)
