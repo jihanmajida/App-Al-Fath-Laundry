7ec1f74bf794b02026b83f6f20d02f9bb09b2e19
jihanmajida <jeehan.majeeda@gmail.com>
2026-03-01 13:43:27 +0700

    base url changed

--- app/src/main/java/com/example/alfathhlaundry/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/alfathhlaundry/data/network/RetrofitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8000/api/"
+    private const val BASE_URL = "https://alfath-laundry.projects.my.id/api"
 
     fun getInstance(context: Context): ApiService {
 
